21cb8ca38cf44f1926f7578d2592b4f1cdd60b3c
FANLOYU <yifanjiang@hotmail.com>
2016-03-04 21:44:48 -0600

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,34 +12,51 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings=['G','PG','PG-13','R']
-    @redirect=0
-    
-    @movies = Movie.all
-
-    if(params[:sort].to_s == 'title')
-      session[:sort] = params[:sort]
-      @movies = @movies.sort_by{|m| m.title}
-    elsif(params[:sort].to_s == 'release_date')
-      session[:sort] = params[:sort]
-      @movies = @movies.sort_by{|m| m.release_date}
-    elsif(session.has_key?(:sort))
-      params[:sort]=session[:sort]
-      @redirect=1
+    @redirect=false
+    if(@checked==nil)
+      @movies=Movie.all
     end
-    
-    
+    #get corresponding ratings data from database
     if(params[:ratings] != nil)
       session[:ratings]=params[:ratings]
-      @movies = @movies.find_all{ |m| params[:ratings].has_key?(m.rating) }
+      array_ratings = params[:ratings].keys
+      @movies = Movie.where(rating: array_ratings)
     elsif(session.has_key?(:ratings))
       params[:ratings]=session[:ratings]
-      @redirect=1
+      @redirect=true
     end
-    
-    if(@redirect==1)
+    #sort the retrieved data by corresponding attribute
+    if(params[:sort].to_s == 'title')
+      session[:sort] = params[:sort]
+      if(@movies==nil)
+        @movies=Movie.order('title')
+      elsif
+        @movies = @movies.sort_by{|m| m.title}
+      end
+    elsif(params[:sort].to_s == 'release_date')
+      session[:sort] = params[:sort]
+      if(@movies==nil)
+        @movies=Movie.order('release_date')
+      elsif
+        @movies = @movies.sort_by{|m| m.release_date}
+      end
+    elsif(session.has_key?(:sort))
+      params[:sort]=session[:sort]
+      @redirect=true
+    end
+    #redirect to index function with filled params
+    if(@redirect)
       redirect_to movies_path(sort: params[:sort], ratings: params[:ratings])
     end
-
+    # set the checked box
+    @checked={}
+    @all_ratings.each { |rating|
+      if params[:ratings] == nil
+        @checked[rating] = false
+      else
+        @checked[rating] = params[:ratings].has_key?(rating)
+      end
+    }
   end
 
   def new
